b69df6b11d7a5de5bffde247303d589c6b1ce728
Shallker Wang <shallker.wang@profero.com>
2013-10-24 00:34:05 +0800

    Move dom-event-shim to index

--- index.js
+++ index.js
@@ -2,8 +2,7 @@
  * Formalize custom inputs into form elements, or at least let them behave like 
  * form elements.
  */
-
-exports.version = '1.0.0';
+require('dom-event-shim');
 
 exports.Radio = require('./lib/radio');
 
